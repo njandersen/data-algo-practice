1682e11de4dd7bba251a0c48487c047f593254c2
njandersen <njandersen22@gmail.com>
2024-12-14 08:24:44 -0500

    Feat: add binary seach

---
+++ javascript/trees/binaryTree.js
@@ -0,0 +1,32 @@
+/*
+    Each node can have either zero, 1 or 2 nodes.
+    Each child can only have one parent. 
+    Perfect binary tree is when the tree is full, meaning each node as two
+    children. PBT is really efficient. Nodes double each level. All the nodes
+    above the bottom level added up and plus 1 will equal the bottom level
+    that means half of our nodes are on the bottom level.  
+*/
+
+class BinaryTreeNode {
+  //todo
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    //todo
+  }
+
+  lookup(val) {
+    //todo
+  }
+
+  remove(val) {
+    //todo
+  }
+}
+
+const tree = new BinarySearchTree();
